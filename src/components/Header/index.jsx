ce7c96060c967845d973efe9ce672b89cf40fad5
aquarius <aquarius@qq.com>
2022-04-03 16:53:06 +0800

    Header组件

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Search,
+  Login,
+  Category
+} from "../index";
+import "./css/index.scss";
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className='header-left'>
+        <svg className="icon" aria-hidden="true">
+          <use xlinkHref="#icon-feiji"></use>
+        </svg>
+        <h1>Aquarius</h1>
+      </div>
+      <div className='header-right'>
+        <Search />
+        <Category />
+        <Login />
+      </div>
+    </div>
+  )
+}
