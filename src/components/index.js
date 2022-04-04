59bc2b13e10a54d3492d5890b7b78e2a0d66287c
aquarius <aquarius@qq.com>
2022-04-04 17:38:56 +0800

    添加组件以及wrap的动态加载组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,20 @@
+import React from "react";
 import Search from "./Search";
 import Login from "./Login";
 import Category from "./Category";
+import Header from "./Header";
+import Content from "./Content";
+import Describe from "./Describe";
+import Lists from "./Lists";
+const Wrap = React.lazy(() => import("./Wrap"));
 
 export {
     Search,
     Login,
-    Category
+    Category,
+    Header,
+    Content,
+    Describe,
+    Lists,
+    Wrap
 }
