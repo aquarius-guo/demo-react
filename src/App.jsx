82058292a1724dea4006b09614dfe3f4a091621b
aquarius <aquarius@qq.com>
2022-04-09 18:42:39 +0800

    使用BrowserRouter包裹组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import { BrowserRouter } from "react-router-dom";
 import { Header, Content } from "./components";
-
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <Content />
-    </>
+    </BrowserRouter>
   )
 }
