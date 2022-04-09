37042a0848f9d34069d94532b0ff65cdedc7cda4
aquarius <aquarius@qq.com>
2022-04-09 18:44:46 +0800

    加载组件

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Spin } from "antd";
+import "./css/index.scss";
+
+export default function index() {
+  return (
+    <div className="example">
+      <Spin />
+    </div>
+  )
+}
+
