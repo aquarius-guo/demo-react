2845e378a55f33d5979aa1b6dca3fc71380c323b
aquarius <aquarius@qq.com>
2022-04-09 18:45:11 +0800

    Wrap布局

--- src/components/Wrap/index.jsx
+++ src/components/Wrap/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useRoutes } from "react-router-dom";
+import routes from "@/pages"
 import "./css/index.scss";
 
 export default function Wrap() {
   return (
     <div className='wrap'>
-        
+      {useRoutes(routes)}
     </div>
   )
 }
