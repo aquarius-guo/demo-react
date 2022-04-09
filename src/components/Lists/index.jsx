57c9d55a859cccf77702a5dc0191ad6f4079d19f
aquarius <aquarius@qq.com>
2022-04-09 18:44:10 +0800

    新增右侧Lists列表

--- src/components/Lists/index.jsx
+++ src/components/Lists/index.jsx
@@ -3,6 +3,20 @@ import "./css/index.scss";
 
 export default function Lists() {
   return (
-    <div className='lists'>Lists</div>
+    <div className='lists'>
+      <h3 className='lists-title'>文章列表</h3>
+      <ul>
+        <li>队列的实现与应用</li>
+        <li>栈的实现与应用</li>
+        <li>什么是 XSS 攻击？</li>
+        <li>集合的实现与应用</li>
+        <li>树的实现</li>
+        <li>什么是作用域？</li>
+        <li>链表的实现与应用</li>
+        <li>变量在作用域链上怎么查找？</li>
+        <li>JavaScript 代码的执行流程</li>
+        <li>图的遍历</li>
+      </ul>
+    </div>
   )
 }
