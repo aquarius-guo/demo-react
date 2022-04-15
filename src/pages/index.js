39a49a5d8f6544e25e1042e024641eabfe440cff
aquarius <aquarius@qq.com>
2022-04-15 18:37:31 +0800

    配置路由组件入口

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,34 @@ import { Navigate } from "react-router-dom"
 import useSuspense from "./HOC";
 const Lists = React.lazy(() => import('./Lists'));
 const Article = React.lazy(() => import('./Article'));
+const Archives = React.lazy(() => import('./Archives'));
+const Categorys = React.lazy(() => import('./Categorys'));
+const About = React.lazy(() => import('./About'));
 
 export default [
-    {
-        path: "/",
-        element: useSuspense(Lists)
-    },
-    {
-        path: "/articles",
-        element: useSuspense(Article)
-    },
-    {
-        path: "*",
-        element: <Navigate to="/" />
-    }
+	{
+ 		path: "/",
+		element: useSuspense(Lists)
+	},
+	{
+		path: "/archives",
+		element: useSuspense(Archives)
+	},
+	{
+		path: "/categorys",
+		element: useSuspense(Categorys)
+	},
+	{
+		path: "/about",
+		element: useSuspense(About)
+	},
+	{
+		path: "/article",
+		element: useSuspense(Article),
+	},
+	{
+		path: "*",
+		element: <Navigate to="/" />
+	}
 ]
 
