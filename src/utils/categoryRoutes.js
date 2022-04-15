1c64d589b07492a726c607c6ab0fa0e0ffa016e9
aquarius <aquarius@qq.com>
2022-04-15 18:37:59 +0800

    路由配置

---
+++ src/utils/categoryRoutes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { HomeOutlined, EditOutlined, TagsOutlined, UserSwitchOutlined } from "@ant-design/icons";
+
+
+const categoryRoutes = [
+    {
+        to: "/",
+        icon: <HomeOutlined />,
+        title: "首页"
+    },
+    {
+        to: "/archives",
+        icon: <EditOutlined />,
+        title: "归档"
+    },
+    {
+        to: "/categorys",
+        icon: <TagsOutlined />,
+        title: "分类"
+    },
+    {
+        to: "/about",
+        icon: <UserSwitchOutlined />,
+        title: "关于"
+    }
+]
+
+export default categoryRoutes;
