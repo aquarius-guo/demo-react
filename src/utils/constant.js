07ea1b77f252733fbc379ec324a9f4884af62019
aquarius <aquarius@qq.com>
2022-04-15 18:38:11 +0800

    常量

---
+++ src/utils/constant.js
@@ -0,0 +1,23 @@
+/**
+ * @description 常量
+ * @author Aquarius
+ * @date 2022-4-15 17:06:03
+ */
+
+
+const CLIENT_ID = '983e05bfbeca45756b74';
+const STATE = 'mm146gglhydvv';
+const BASE_URL = "https://github.com";
+const CLIENT_SECRET = "d1fca7be906d07f67a76bce16a251a8d8f90d50c";
+const REDIRECT_URL = "http://localhost:8080";
+// github oauth url
+const GITHUB_URL_OAUTH = `${BASE_URL}/login/oauth/authorize?scope=email&allow_signup=true&client_id=${CLIENT_ID}&state=${STATE}`
+
+export {
+    GITHUB_URL_OAUTH,
+    BASE_URL,
+    STATE,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URL
+}
