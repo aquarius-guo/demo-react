3f30bdecb6e7dd66d450bedac77387e2145cff18
aquarius <aquarius@qq.com>
2022-04-15 18:39:09 +0800

    使用auoth登录github

---
+++ src/api/login.js
@@ -0,0 +1,15 @@
+/**
+ * @description 用户登录
+ * @author Aquarius
+ * @date 2022-4-15 18:14:12
+ */
+import axios from "@/utils/http";
+import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URL } from "@/utils/constant";
+
+// 登录
+export const userLogin = async (code) => await axios.post("/api/login/oauth/access_token", {
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    redirect_uri: REDIRECT_URL,
+    code,
+})
