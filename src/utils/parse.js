532145419b44e2b3510978a1ef507bb159f647b9
Aquarius <3115803957@qq.com>
2022-05-05 17:00:46 +0800

    解析token

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -4,7 +4,26 @@
  * @date 2022-4-15 18:25:49
  */
 
+import { SESSION_ID } from "./constant";
+
+// 解析 token
 export const parseToken = (token) => {
-    const OAUTH_TOKEN = token.split("&")[1].split("=")[1];
+    const OAUTH_TOKEN = token.split("&")[0].split("=")[1];
+    setToken(OAUTH_TOKEN);
     return OAUTH_TOKEN;
+}
+
+// 设置 token
+export const setToken = (OAUTH_TOKEN) => {
+    window.localStorage.setItem(SESSION_ID, OAUTH_TOKEN);
+}
+
+// 获取 token
+export const getToken = () => {
+    return window.localStorage.getItem(SESSION_ID) || "";
+}
+
+// 删除 token
+export const removeToken = () => {
+    return window.localStorage.removeItem(SESSION_ID)
 }
