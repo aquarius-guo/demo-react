3846fe9ec078f50a74ae76edd094c2e0cb03af0a
Aquarius <3115803957@qq.com>
2022-05-05 16:59:03 +0800

    登录组件

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,32 +1,78 @@
-import React, { useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { Dropdown } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { Dropdown, message } from 'antd';
 import { UserOutlined } from "@ant-design/icons";
-import './css/index.scss';
-import { userLogin } from '@/api/login';
+import { userLogin, getUserInfo } from '@/api/login';
 import { GITHUB_URL_OAUTH } from '@/utils/constant';
-import { parseToken } from '@/utils/parse';
+import { parseToken, getToken, removeToken } from '@/utils/parse';
+import './css/index.scss';
+
 
 export default function Login() {
   // 获取 search 参数
   const [search] = useSearchParams();
-  const code = search.get("code");
+  const navigate = useNavigate();
+  // 存储用户头像和名称
+  const [_, setUserName] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [isLogin, setIsLogin] = useState(getToken());
+
+
   // 跳转授权页面
   const handleClick = () => {
     window.location.href = GITHUB_URL_OAUTH
   }
+  // 退出登录
+  const handleLogOut = () => {
+    // 删除 token
+    removeToken();
+    setUserName("");
+    setAvatar("");
+    setIsLogin("");
+    navigate("/");
+  }
+
   // 登录
   useEffect(() => {
-    async function getUserInfo(code) {
-      const data = await userLogin(code);
-      // 获取的 token
-      const token = parseToken(data);
+    async function getGitToken(code) {
+      try {
+        const data = await userLogin(code);
+        // 获取的 token
+        parseToken(data);
+        setIsLogin(data);
+        // 跳转到首页
+        navigate("/");
+      } catch (error) {
+        message.error("请求失败, 情稍后再试");
+        navigate("/");
+      }
     }
+    const code = search.get("code");
     if (code) {
-      getUserInfo(code);
+      getGitToken(code);
     }
-  });
-  const loginShow = (
+  }, []);
+
+  // 获取用户信息
+  useEffect(() => {
+    async function getUser() {
+      const { login, avatar_url } = await getUserInfo();
+      setUserName(login);
+      setAvatar(avatar_url);
+    };
+    if (isLogin) {
+      getUser();
+    }
+  }, [isLogin]);
+
+  const loginShow = isLogin ? (
+    <p className='header-info' onClick={handleLogOut}>
+      <span>退出</span>
+      <svg className="icon login-icon" aria-hidden="true">
+        <use xlinkHref="#icon-denglu"></use>
+      </svg>
+    </p>
+  ) : (
     <p className='header-info' onClick={handleClick}>
       <span>登录</span>
       <svg className="icon login-icon" aria-hidden="true">
@@ -38,10 +84,17 @@ export default function Login() {
   return (
     <Dropdown overlay={loginShow}>
       <div className='header-login'>
-        <div className='header-avatar'>
-          <UserOutlined />
-        </div>
+        {
+          isLogin ? (
+            <img src={avatar} alt="" />
+          ) : (
+            <div className='header-avatar'>
+              <UserOutlined />
+            </div>
+          )
+        }
+
       </div>
-    </Dropdown>
+    </Dropdown >
   )
 }
