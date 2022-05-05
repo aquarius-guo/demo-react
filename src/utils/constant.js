68cd7fd58e7a0d30045416054ad75d5aca55e6a3
Aquarius <3115803957@qq.com>
2022-05-05 16:59:59 +0800

    常量

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,6 +5,7 @@
  */
 
 
+// github OAuth
 const CLIENT_ID = '983e05bfbeca45756b74';
 const STATE = 'mm146gglhydvv';
 const BASE_URL = "https://github.com";
@@ -12,6 +13,8 @@ const CLIENT_SECRET = "d1fca7be906d07f67a76bce16a251a8d8f90d50c";
 const REDIRECT_URL = "http://localhost:8080";
 // github oauth url
 const GITHUB_URL_OAUTH = `${BASE_URL}/login/oauth/authorize?scope=email&allow_signup=true&client_id=${CLIENT_ID}&state=${STATE}`
+// session_id
+const SESSION_ID = "__SESSION_GITHUB_TOKEN";
 
 export {
     GITHUB_URL_OAUTH,
@@ -19,5 +22,6 @@ export {
     STATE,
     CLIENT_ID,
     CLIENT_SECRET,
-    REDIRECT_URL
+    REDIRECT_URL,
+    SESSION_ID
 }
