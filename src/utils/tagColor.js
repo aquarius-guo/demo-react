ec393605d9ea598189325cf55d69cb0ceda0e366
Aquarius <3115803957@qq.com>
2022-05-05 17:01:17 +0800

    tag颜色

---
+++ src/utils/tagColor.js
@@ -0,0 +1,25 @@
+/**
+ * @description tag list
+ * @author Aquarius
+ * @time 2022年4月23日16:23:42
+ */
+const TagList = [
+  "magenta",
+  "red",
+  "volcano",
+  "orange",
+  "gold",
+  "lime",
+  "green",
+  "cyan",
+  "blue",
+  "geekblue",
+  "purple",
+  "success",
+  "processing",
+  "error",
+  "warning",
+  "default",
+  "cyan",
+]
+export default TagList;
